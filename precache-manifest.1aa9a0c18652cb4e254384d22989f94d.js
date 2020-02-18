79bd0ef457f10080aa8f887d098c80bbedb1b66b
Islamova Akmaral <akmaralslmv@gmail.com>
2020-02-18 23:01:42 +0600

    Updates

---
+++ precache-manifest.1aa9a0c18652cb4e254384d22989f94d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294a887fb0ee3698b8f8f3abb355e842",
+    "url": "/reactive-tic-tac/index.html"
+  },
+  {
+    "revision": "98b97babadc20ea74cda",
+    "url": "/reactive-tic-tac/static/css/main.6e9e47dc.chunk.css"
+  },
+  {
+    "revision": "cac633b125f489309d89",
+    "url": "/reactive-tic-tac/static/js/2.55099bc3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactive-tic-tac/static/js/2.55099bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b97babadc20ea74cda",
+    "url": "/reactive-tic-tac/static/js/main.7680b309.chunk.js"
+  },
+  {
+    "revision": "0eb30d39685cb2021575",
+    "url": "/reactive-tic-tac/static/js/runtime-main.10bc9c25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactive-tic-tac/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5bdf03437432bce0931ced14e716755a",
+    "url": "/reactive-tic-tac/static/media/nolik.5bdf0343.png"
+  },
+  {
+    "revision": "22254c081e92120d967e0e05b0f14f96",
+    "url": "/reactive-tic-tac/static/media/simka.22254c08.png"
+  }
+]);
